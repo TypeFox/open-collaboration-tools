5f537c34ee5a7cc88133fda15158b1ea37758c2c
Jonah Iden <jonah.iden@typefox.io>
2024-09-12 13:50:08 +0200

    package setup
    Signed-off-by: Jonah Iden <jonah.iden@typefox.io>

---
+++ packages/open-collaboration-intelliJ/build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+  id("java")
+  id("org.jetbrains.kotlin.jvm") version "1.9.25"
+  id("org.jetbrains.intellij") version "1.17.4"
+}
+
+group = "org.typefox"
+version = "1.0-SNAPSHOT"
+
+repositories {
+  mavenCentral()
+}
+
+// Configure Gradle IntelliJ Plugin
+// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
+intellij {
+  version.set("2023.2.6")
+  type.set("IC") // Target IDE Platform
+
+  plugins.set(listOf(/* Plugin Dependencies */))
+}
+
+tasks {
+  // Set the JVM compatibility versions
+  withType<JavaCompile> {
+    sourceCompatibility = "17"
+    targetCompatibility = "17"
+  }
+  withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions.jvmTarget = "17"
+  }
+
+  patchPluginXml {
+    sinceBuild.set("232")
+    untilBuild.set("242.*")
+  }
+
+  signPlugin {
+    certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+    privateKey.set(System.getenv("PRIVATE_KEY"))
+    password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+  }
+
+  publishPlugin {
+    token.set(System.getenv("PUBLISH_TOKEN"))
+  }
+}
